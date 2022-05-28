ed2a06b8674e2b8b9460185c46d091032428e893
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-28 10:14:04 -0300

    .

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,4 +1,12 @@
-   import React from 'react';
-   import ReactDOM from 'react-dom';
-   
-   import { Button } from '@ds.e/react';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Button from './../../../packages/react/src/atoms/Button/Button';
+
+const rootElement = document.querySelector("#root");
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement)
+root.render(
+   <React.StrictMode>
++    <Button label="Example Button"/>
++  </React.StrictMode>
+   );
